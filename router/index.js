1a98d28b9b1857992e864b9d05221a88a5e52ff6
Luke Malinowski <malinowski.luke123@gmail.com>
2021-02-21 22:29:34 -0500

    added delete endpoint

--- router/index.js
+++ router/index.js
@@ -1,25 +1,30 @@
-const express = require("express");
-const Router = express.Router();
+const express = require('express')
+const Router = express.Router()
 
-const Product = require("../controllers/product");
-const User = require("../controllers/user");
-const Validate = require("../middleware/validate");
+const Product = require('../controllers/product')
+const User = require('../controllers/user')
+const Validate = require('../middleware/validate')
 
-Router.route("/").get((req, res) => res.status(200).send("OK!"));
+Router.route('/').get((req, res) => res.status(200).send('OK!'))
 
-Router.route("/product").post(Validate.bodyChannelID, Product.getProducts);
-Router.route("/product/add").post(Product.getProductInfo);
+// products
+Router.route('/product/:channel_id').get(
+  Validate.paramChannelID,
+  Product.getProducts
+)
+Router.route('/product').post(Product.postProduct)
+Router.route('/product/:product_id').delete(Product.deleteProduct)
 
-Router.route("/user/:channel_id").get(Validate.paramChannelID, User.getUser);
-Router.route("/user/new").post(
-    Validate.bodyChannelID,
-    Validate.bodyClientID,
-    User.createUser
-);
+// user
+Router.route('/user/:channel_id').get(Validate.paramChannelID, User.getUser)
+Router.route('/user/new').post(
+  Validate.bodyChannelID,
+  Validate.bodyClientID,
+  User.createUser
+)
+Router.route('/user/delete').post(
+  Validate.bodyClientID,
+  User.markUserForDeletion
+)
 
-Router.route("/user/delete").post(
-    Validate.bodyClientID,
-    User.markUserForDeletion
-);
-
-module.exports = Router;
+module.exports = Router
