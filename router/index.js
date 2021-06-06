e13241a82e42ba672ea497596c05342982488aef
malinowskil123 <malinowski.luke123@gmail.com>
2021-06-06 18:21:09 -0400

    refactor

--- router/index.js
+++ router/index.js
@@ -1,27 +1,36 @@
-const express = require("express")
+const express = require('express')
 const Router = express.Router()
 
-const Product = require("../controllers/product")
-const User = require("../controllers/user")
+const {
+  getProducts,
+  postProduct,
+  scrapeProduct,
+  deleteProduct,
+} = require('../controllers/product')
+
+const {
+  createUser,
+  getUser,
+  markUserForDeletion,
+} = require('../controllers/user')
 
 // ping
-Router.route("/").get((req, res) => res.status(200).send("OK!"))
+Router.route('/').get((req, res) => res.status(200).send('OK!'))
 
 // product
-Router.route("/product/scrape/:channel_id").post(Product.scrapeProduct)
+Router.route('/product/scrape/:channel_id').post(scrapeProduct)
 
-Router.route("/product/:channel_id").get(	Product.getProducts)
+Router.route('/product/:channel_id').get(getProducts)
 
-Router.route("/product/:channel_id").post(Product.postProduct)
+Router.route('/product/:channel_id').post(postProduct)
 
-Router.route("/product/:channel_id/:product_id").delete(Product.deleteProduct)
-
+Router.route('/product/:channel_id/:product_id').delete(deleteProduct)
 
 // user
-Router.route("/user").post(User.createUser)
+Router.route('/user').post(createUser)
 
-Router.route("/user/:channel_id").get(User.getUser)
+Router.route('/user/:channel_id').get(getUser)
 
-Router.route("/user/:channel_id").delete(User.markUserForDeletion)
+Router.route('/user/:channel_id').delete(markUserForDeletion)
 
 module.exports = Router
